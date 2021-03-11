4699f6e02a7f7f39e6466896557001c883392603
alexpodelinskii <nikitik00787@gmail.com>
2021-03-12 00:25:13 +0300

    docs: update my solution
    Signed-off-by: alexpodelinskii <nikitik00787@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (!number) return "zero";
+
+    let arr = ["",
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        "sixteen",
+        'seventeen',
+        'eighteen',
+        'nineteen',];
+
+    let arr2 = ["", 'one', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 20) return arr[number];
+    let number1 = "" + number
+    let arrNum = number1.split("");
+    arrNum = arrNum.map(el => +el);
+    let res
+    if (number < 100) {
+        res = arr2[arrNum[0]] + " " + arr[arrNum[1]];
+    } else if ((number % 100) < 20) {
+        res = arr[arrNum[0]] + " hundred " + arr[(arrNum[1] * 10 + arrNum[2])];
+    } else {
+        res = arr[arrNum[0]] + " hundred " + arr2[arrNum[1]] + " " + arr[arrNum[2]];
+    }
+    return res.trim()
+
 }
+
